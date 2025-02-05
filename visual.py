2c8684eca36a5c20606f33bb3c49903c1564b64d
Mobilizes <fernando142311@gmail.com>
2025-02-06 00:42:28 +0700

    feat: add 3d and com trajectory vis

--- visual.py
+++ visual.py
@@ -1,6 +1,3 @@
-import math
-from decimal import Decimal as D
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -9,12 +6,56 @@ from LIPM_3D import LIPM3D
 
 class LIPM3D_Visual:
     def __init__(self, lipm: LIPM3D, nrow=2, ncol=1):
+        self.com_history = []
         self.lipm = lipm
         self.nrow = nrow
         self.ncol = ncol
 
-    def project_walk_pattern(self, x_t_history, index):
+    def project_3d_gait(self, index):
         lipm = self.lipm
+        com_history = self.com_history
+
+        fig = plt.figure("LIPM")
+
+        ax = fig.add_subplot(self.nrow, self.ncol, index, projection="3d")
+
+        ax.set_xlim(0, 1)
+        ax.set_ylim(-0.5, 0.5)
+
+        (left_leg,) = ax.plot(
+            [float(lipm.x_t), float(lipm.left_foot_pos[0])],
+            [float(lipm.y_t), float(lipm.left_foot_pos[1])],
+            zs=[float(lipm.z_c), float(lipm.left_foot_pos[2])],
+            color="cyan",
+            linewidth=2.0
+        )
+
+        (right_leg,) = ax.plot(
+            [float(lipm.x_t), float(lipm.right_foot_pos[0])],
+            [float(lipm.y_t), float(lipm.right_foot_pos[1])],
+            zs=[float(lipm.z_c), float(lipm.right_foot_pos[2])],
+            color="blue",
+            linewidth=2.0
+        )
+
+        (com,) = ax.plot(
+            float(lipm.x_t),
+            float(lipm.y_t),
+            "o",
+            color="red",
+            zs=float(lipm.z_c),
+        )
+
+        (com_traj,) = ax.plot(
+            [float(row[0]) for row in com_history],
+            [float(row[1]) for row in com_history],
+            zs=0,
+            color="red"
+        )
+
+    def project_walk_pattern(self, index):
+        lipm = self.lipm
+        com_history = self.com_history
 
         fig = plt.figure("LIPM")
 
@@ -23,7 +64,7 @@ class LIPM3D_Visual:
         ax.set_xlabel("x (m)")
         ax.set_ylabel("y (m)")
 
-        # x_t_history.append((lipm.x_t, lipm.y_t))
+        # com_history.append((lipm.x_t, lipm.y_t))
         # for (x_t, y_t) in x_t_history:
         #     ax.plot(
         #         x_t,
@@ -46,6 +87,11 @@ class LIPM3D_Visual:
             color="red",
             label=str(float(lipm.t))
         )
+        (com_traj,) = ax.plot(
+            [float(row[0]) for row in com_history],
+            [float(row[1]) for row in com_history],
+            color="red"
+        )
 
         (mod_p,) = ax.plot(lipm.mod_p_x, lipm.mod_p_y, "o", color="gray")
         (left_foot_pos,) = ax.plot(
@@ -62,11 +108,10 @@ class LIPM3D_Visual:
         )
         # print((lipm.mod_p_y - lipm.start_swing_foot[1]))
 
-        dist = math.sqrt(float(lipm.right_foot_pos[0]-lipm.left_foot_pos[0])**2 + float(lipm.right_foot_pos[1] - lipm.left_foot_pos[1])**2)
-        print(dist)
+        # dist = math.sqrt(float(lipm.right_foot_pos[0]-lipm.left_foot_pos[0])**2 + float(lipm.right_foot_pos[1] - lipm.left_foot_pos[1])**2)
+        # print(f"{dist}, {lipm.s_theta_1}, {lipm.s_theta_1 / D(np.pi) * D("180.0")}")
 
         ax.set_xlim(-0.5, 2.0)
         ax.set_ylim(-0.5, 0.5)
 
         plt.legend()
-        plt.show()
