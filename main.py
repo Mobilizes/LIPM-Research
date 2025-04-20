b702773d464828bb3df3fef859830f817ddbc396
Mobilizes <fernando142311@gmail.com>
2025-04-21 04:22:49 +0700

    refactor: add foot history

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import time
 
 def main():
     model = Walk()
+
     com_x = []
     com_y = []
     zmp_x = []
@@ -20,69 +21,48 @@ def main():
     p_ref_x = []
     p_ref_y = []
 
-    while len(model.p_ref) > 970:
+    max_data = 100
+    while len(left_foot_x) <= max_data:
         model.step()
 
-        com_x.append(model.com[0][0])
-        com_y.append(model.com[1][0])
-        zmp_x.append(model.zmp[0])
-        zmp_y.append(model.zmp[1])
+        left_foot_x.append(model.left_foot[0][0])
+        left_foot_y.append(model.left_foot[0][1])
+        right_foot_x.append(model.right_foot[0][0])
+        right_foot_y.append(model.right_foot[0][1])
 
-        left_foot_x.append(model.left_foot[0])
-        left_foot_y.append(model.left_foot[1])
-        right_foot_x.append(model.right_foot[0])
-        right_foot_y.append(model.right_foot[1])
         p_ref_x.append(model.p_ref[0][0])
         p_ref_y.append(model.p_ref[0][1])
 
-        # fig, ax = plt.subplots()
-        # com, = ax.plot(com_x[-1], com_y[-1], 'bo', markersize=8, label='Current CoM')
-        # line, = ax.plot(com_x, com_y, 'r-')
-        # # for p_ref in model.p_ref:
-        # #     ax.plot(p_ref[0], p_ref[1], 'bo', markersize=8, color="green")
-        # # ref, = ax.plot(model.p_ref[:, 0], model.p_ref[:, 1], 'b', color="red")
-        # ax.set_xlabel('X Position [m]')
-        # ax.set_ylabel('Y Position [m]')
-        # ax.set_title('Center of Mass Trajectory')
-        # ax.grid(True)
-        # ax.legend()
-        #
-        # plt.show()
+        # print(f"Start Swing: {model.start_swing_foot}")
+        print(f"Left Foot: {model.left_foot}")
+        print(f"Right Foot: {model.right_foot}")
+        print(f"Support State: {model.support_state}")
+        # print(f"State: {model.state}")
+        # print(f"Phase time: {model.t_phase}")
 
-    zmp_x[0] = float(zmp_x[0])
-    zmp_y[0] = float(zmp_y[0])
+        plt.plot(model.left_foot[0][0], model.left_foot[0][1], 'bo')
+        plt.plot(model.right_foot[0][0], model.right_foot[0][1], 'ro')
+        plt.plot(model.p_ref[0][0], model.p_ref[0][1], 'go')
 
-    fig, ax = plt.subplots()
-    com, = ax.plot(com_x, com_y, 'bo', markersize=8, label='Current CoM')
-    zmp, = ax.plot(zmp_x, zmp_y, 'go', markersize=8, label='ZMP')
-    line, = ax.plot([], [], 'r-')
-    p_ref_line = ax.plot(p_ref_x, p_ref_y, 'o', markersize=8, label="Reference", color="gray")[0]
-    left_foot_line = ax.plot(left_foot_x, left_foot_y, 'o', markersize=8, label='Left Foot', color="red")[0]
-    right_foot_line = ax.plot(right_foot_x, right_foot_y, 'o', markersize=8, label='Right Foot', color="maroon")[0]
-    ax.set_xlabel('X Position [m]')
-    ax.set_ylabel('Y Position [m]')
-    ax.set_title('Center of Mass Trajectory')
-    ax.grid(True)
-    ax.legend()
+        plt.show()
 
-    def animate(i):
-        com.set_data(com_x[i], com_y[i])
-        zmp.set_data(zmp_x[i], zmp_y[i])
-        line.set_data(com_x[:i+1], com_y[:i+1])
-        p_ref_line.set_data(p_ref_x[i], p_ref_y[i])
-        left_foot_line.set_data(left_foot_x[i], left_foot_y[i])
-        right_foot_line.set_data(right_foot_x[i], right_foot_y[i])
 
-        return com, zmp, line, p_ref_line, left_foot_line, right_foot_line
-
-    ani = animation.FuncAnimation(
-        fig, animate,
-        frames=len(com_x),
-        interval=30,
-        blit=True
-    )
-
-    plt.show()
+    # fig, ax = plt.subplots()
+    #
+    # left_foot_plot, = ax.plot(left_foot_x, left_foot_y, 'bo', label='Left Foot')
+    # right_foot_plot, = ax.plot(right_foot_x, right_foot_y, 'ro', label='Right Foot')
+    # p_ref_plot, = ax.plot(p_ref_x, p_ref_y, 'go', label='Reference Point')
+    # ax.legend()
+    #
+    # def update(i):
+    #     left_foot_plot.set_data(left_foot_x[i], left_foot_y[i])
+    #     right_foot_plot.set_data(right_foot_x[i], right_foot_y[i])
+    #     p_ref_plot.set_data(p_ref_x[i], p_ref_y[i])
+    #
+    #     return left_foot_plot, right_foot_plot, p_ref_plot
+    #
+    # ani = animation.FuncAnimation(fig, update, frames=max_data, blit=True)
+    # plt.show()
 
 if __name__ == "__main__":
     main()
--- walk.py
+++ walk.py
@@ -16,7 +16,7 @@ class Walk:
             "foot_height": 0.1,
             "z_c": 1.2,
             "preview_steps": 10,
-            "support_leg": "right",
+            "support_state": "right",
             "x_speed": 2.0,
             "y_speed": 0.0,
             "a_speed": 0.0,
@@ -40,13 +40,11 @@ class Walk:
         self.b = np.array([self.dt**3 / 3.0, self.dt**2 / 2.0, self.dt])[np.newaxis].T
         self.c = np.array([1.0, 0.0, -self.z_c / 9.81])[np.newaxis]
 
-        self.left_foot = opt["left_foot"]
-        self.right_foot = opt["right_foot"]
-        self.support_leg = opt["support_leg"]
+        self.left_foot = [opt["left_foot"]]
+        self.right_foot = [opt["right_foot"]]
+        self.support_state = [opt["support_state"]]
 
-        self.start_swing_foot = (
-            self.left_foot if self.support_leg == "right" else self.right_foot
-        )
+        self.start_swing_foot = self.get_swing_foot()
 
         self.preview_steps = opt["preview_steps"]
 
@@ -54,22 +52,37 @@ class Walk:
 
         self.speed = [opt["x_speed"], opt["y_speed"], opt["a_speed"]]
 
-        self.lipm_init = True
+        self.initial = True
+        self.initial_lipm = True
         self.update_p_ref()
 
     def get_support_foot(self):
-        return self.left_foot if self.support_leg == "left" else self.right_foot
+        return self.left_foot[-1] if self.support_state[-1] == "left" else self.right_foot[-1]
 
     def get_swing_foot(self):
-        return self.left_foot if self.support_leg == "right" else self.right_foot
+        return self.left_foot[-1] if self.support_state[-1] == "right" else self.right_foot[-1]
 
-    def set_swing_foot(self, foot):
-        if self.support_leg == "right":
-            self.left_foot = foot
-        elif self.support_leg == "left":
-            self.right_foot = foot
+    def update_foot(self, swing_foot, pop=True, switch=False):
+        if switch:
+            self.support_state.append("left" if self.support_state[-1] == "right" else "right")
         else:
-            raise Exception("Unknown support leg : ", self.support_leg)
+            self.support_state.append(self.support_state[-1])
+        self.support_state.pop(0) if pop else None
+
+        if self.support_state[-1] == "right":
+            self.left_foot.append(swing_foot)
+            self.left_foot.pop(0) if pop else None
+
+            self.right_foot.append(self.get_support_foot())
+            self.right_foot.pop(0) if pop else None
+        elif self.support_state[-1] == "left":
+            self.right_foot.append(swing_foot)
+            self.right_foot.pop(0) if pop else None
+
+            self.left_foot.append(self.get_support_foot())
+            self.left_foot.pop(0) if pop else None
+        else:
+            raise Exception("Unknown support leg : ", self.support_state[-1])
 
     def update_p_ref(self):
         curr_p_ref = []
@@ -80,33 +93,39 @@ class Walk:
 
     def update_mpc(self):
         problem = placo.Problem()
-        if self.lipm_init:
-            self.lipm = placo.LIPM(
-                problem,
-                self.dt,
-                self.preview_steps,
-                0.0,
-                np.array([
-                    (self.left_foot[0] + self.right_foot[0]) / 2.0,
-                    (self.left_foot[1] + self.right_foot[1]) / 2.0,
-                ]),
-                np.array([0., 0.]),
-                np.array([0., 0.]),
-            )
-        else:
-            self.lipm = placo.LIPM.build_LIPM_from_previous(problem, self.dt, self.preview_steps, 0., self.lipm)
-
-        support_foot = self.get_support_foot()
-        support_polygon = np.array([
-            [support_foot[0] - self.foot_width / 2., support_foot[1] + self.foot_height / 2.],
-            [support_foot[0] + self.foot_width / 2., support_foot[1] + self.foot_height / 2.],
-            [support_foot[0] + self.foot_width / 2., support_foot[1] - self.foot_height / 2.],
-            [support_foot[0] - self.foot_width / 2., support_foot[1] - self.foot_height / 2.],
-        ])
-
-        self.t_preview = 0.
+        # if self.initial_lipm:
+        #     self.lipm = placo.LIPM(
+        #         problem,
+        #         self.dt,
+        #         self.preview_steps,
+        #         0.0,
+        #         np.array([
+        #             (self.left_foot[-1][0] + self.right_foot[-1][0]) / 2.0,
+        #             (self.left_foot[-1][1] + self.right_foot[-1][1]) / 2.0,
+        #         ]).reshape(2, 1),
+        #         np.array([0., 0.]).reshape(2, 1),
+        #         np.array([0., 0.]).reshape(2, 1),
+        #     )
+        #     self.initial_lipm = False
+        # else: self.lipm = placo.LIPM.build_LIPM_from_previous(problem, self.dt, self.preview_steps, 0., self.lipm)
+        #
+        # support_foot = self.get_support_foot()
+        # support_polygon = np.array([
+        #     [support_foot[0] - self.foot_width / 2., support_foot[1] + self.foot_height / 2.],
+        #     [support_foot[0] + self.foot_width / 2., support_foot[1] + self.foot_height / 2.],
+        #     [support_foot[0] + self.foot_width / 2., support_foot[1] - self.foot_height / 2.],
+        #     [support_foot[0] - self.foot_width / 2., support_foot[1] - self.foot_height / 2.],
+        # ])
 
     def run_ssp(self):
+        if self.t_phase >= self.t_sup:
+            self.state = "dsp"
+            self.t_phase = 0.
+            self.start_swing_foot = self.get_swing_foot()
+            self.p_ref = np.delete(self.p_ref, 0, 0)
+            # self.update_p_ref()
+            return
+
         progress = min(1.0, self.t_phase / self.t_sup)
 
         curr_swing_foot = (1 - progress) * self.start_swing_foot + progress * self.p_ref[0]
@@ -115,32 +134,38 @@ class Walk:
         else:
             curr_swing_foot[2] = (1 - progress) / 0.5 * 0.2
 
-        self.set_swing_foot(curr_swing_foot)
+        self.update_foot(
+            curr_swing_foot, not self.initial, self.t_phase + self.dt >= self.t_sup
+        )
 
         self.t_phase += self.dt
-        if self.t_phase >= self.t_sup:
-            self.state = "dsp"
-            self.t_phase = 0.
-            self.support_leg = "left" if self.support_leg == "right" else "right"
-            self.start_swing_foot = self.get_swing_foot()
-            self.p_ref = np.delete(self.p_ref, 0, 0)
-            # self.update_p_ref()
 
     def run_dsp(self):
-        self.t_phase += self.dt
-        if self.t_phase >= self.t_sup:
+        if self.t_phase >= self.t_dbl:
             self.state = "ssp"
             self.t_phase = 0.
+            return
+
+        self.update_foot(self.get_swing_foot(), not self.initial)
+
+        self.t_phase += self.dt
 
     def step(self):
-        self.t_preview += self.dt
+        if (
+            len(self.left_foot) >= self.preview_steps
+            and len(self.right_foot) >= self.preview_steps
+        ):
+            self.initial = False
+
+        self.t_preview += self.dt * (1 if not self.initial else 0)
         if self.t_preview >= self.t_mpc:
             self.update_mpc()
+            self.t_preview = 0.
 
+        if self.state == "dsp":
+            self.run_dsp()
         if self.state == "ssp":
             self.run_ssp()
-        elif self.state == "dsp":
-            self.run_dsp()
-        else:
+        if not any(state in self.state for state in ["ssp", "dsp"]):
             raise Exception("Unknown state : ", self.state)
 
